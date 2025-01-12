ab9247ad428fe455b5cd3d3fa1c109b7188e3b46
Yordan Romeev <yordan.romeev@gmail.com>
2025-01-12 12:37:12 +0200

    Refactor tasks queries into 1 file

---
+++ SQL Queries.sql
@@ -0,0 +1,10 @@
+--Task 1 Creating a Database and Table
+
+CREATE TABLE School
+(
+    StudentID INT Primary Key NOT NULL,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Age INT,
+    Grade VARCHAR(10)
+)
