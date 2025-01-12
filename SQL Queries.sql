20a37872386242b2e8e04c09710319866843fda5
Yordan Romeev <yordan.romeev@gmail.com>
2025-01-12 12:52:06 +0200

    Add Task 2 - Inserting Data

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -1,4 +1,4 @@
---Task 1 Creating a Database and Table
+-- Task 1 Creating a Database and Table
 
 CREATE TABLE School
 (
@@ -8,3 +8,13 @@ CREATE TABLE School
     Age INT,
     Grade VARCHAR(10)
 )
+
+-- Task 2 Inserting Data
+
+INSERT INTO School (StudentID, FirstName, LastName, Age, Grade)
+VALUES  (1, 'John', 'Doe', 15, '10th'),
+        (2, 'Jane', 'Austin', 15, '10th'),
+        (3, 'Peter', 'Norton', 14, '9th'),
+        (4, 'Jack', 'Smile', 14, '9th'),
+        (5, 'John', 'Wick', 13, '8th')
+
