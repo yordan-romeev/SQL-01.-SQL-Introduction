ff860a4f8502194b4cb50c679ff4051ad0a4f83a
Yordan Romeev <yordan.romeev@gmail.com>
2025-01-12 13:11:49 +0200

    Fixed Task 1 and Refactored All Tasks

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -1,6 +1,10 @@
 -- Task 1 Creating a Database and Table
 
-CREATE TABLE School
+--Create a database called School
+CREATE DATABASE School
+
+--Inside the database, create a table named Students with the following columns
+CREATE TABLE Students
 (
     StudentID INT Primary Key NOT NULL,
     FirstName VARCHAR(50) NOT NULL,
@@ -11,7 +15,7 @@ CREATE TABLE School
 
 -- Task 2 Inserting Data
 
-INSERT INTO School (StudentID, FirstName, LastName, Age, Grade)
+INSERT INTO Students (StudentID, FirstName, LastName, Age, Grade)
 VALUES  (1, 'John', 'Doe', 15, '10th'),
         (2, 'Jane', 'Austin', 15, '10th'),
         (3, 'Peter', 'Norton', 14, '9th'),
@@ -21,16 +25,17 @@ VALUES  (1, 'John', 'Doe', 15, '10th'),
 -- Task 3 Querying Data
 
 --Write a query to retrieve all columns from the Students table.
-SELECT * FROM School
+SELECT * FROM Students
 
 --Retrieve only the FirstName and Grade of students whose Age is greater than 14
 SELECT FirstName, Grade 
-FROM School
+FROM Students
 WHERE Age > 14
 
 -- Task 4 Updating Data
 
-UPDATE School
+--Update the grade of a specific student. 
+UPDATE Students
 SET Grade = '11th'
 WHERE StudentID = 2;
 
@@ -39,7 +44,5 @@ WHERE StudentID = 2;
 --Delete a student record where the StudentID is 3.
 --Be sure to use the WHERE clause to avoid deleting all rows accidentally.
 
-DELETE FROM School
+DELETE FROM Students
 WHERE StudentID = 3
-
-
